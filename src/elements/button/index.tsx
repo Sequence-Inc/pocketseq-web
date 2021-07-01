b5cbeefdec4bf9c96644c22ba65e6602c7701010
vnshgrg <ghale.avinash@gmail.com>
2021-07-01 13:58:58 +0900

    fixed secondary button ring color

--- src/elements/button/index.tsx
+++ src/elements/button/index.tsx
@@ -15,7 +15,7 @@ const Button = (props: ButtonProps) => {
             className={`${
                 variant === "primary"
                     ? "text-white bg-primary hover:bg-primaryHover focus:ring-primary"
-                    : "text-gray-400 bg-gray-100 hover:bg-gray-200 focus:bg-gray-200"
+                    : "text-gray-400 bg-gray-100 hover:bg-gray-200 focus:ring-gray-200"
             } w-full flex items-center justify-center p-2 border border-transparent text-xs font-medium rounded shadow-sm focus: outline-none focus:ring-2 focus:ring-offset-2 ${
                 className && className
             } `}
