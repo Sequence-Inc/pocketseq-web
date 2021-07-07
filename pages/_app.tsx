4108e67d788545092a476c2ed8977038a2fed53a
vnshgrg <ghale.avinash@gmail.com>
2021-07-07 10:11:20 +0900

    removed tailwindcss.css import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 // tailwind css
-import "tailwindcss/tailwind.css";
+// import "tailwindcss/tailwind.css";
 import "@style/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
