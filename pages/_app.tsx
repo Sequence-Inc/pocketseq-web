bb8f91efe0cd3a579a45fd0a06ed111f0cf6a3ea
0blank <sumangauchan5@gmail.com>
2021-08-17 22:45:32 +0545

    added route gaurd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,18 +6,23 @@ import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "src/apollo/apollo";
 import { useRouter } from "next/router";
 import Home from ".";
+import { isAuthenticated } from "src/utils/auth";
+import { redirect } from "next/dist/next-server/server/api-utils";
 
 function MyApp({ Component, pageProps }: AppProps) {
     const apolloClient = useApollo(pageProps.initialApolloState);
     const role: string = "admin";
     let allowed = true;
     const router = useRouter();
-    if (router.pathname.startsWith("/host") && role !== "host") {
+    if (router.pathname.startsWith("/user-host") && !isAuthenticated()) {
         allowed = false;
     }
-    if (router.pathname.startsWith("/admin") && role !== "admin") {
-        allowed = false;
-    }
+    // if (router.pathname.startsWith("/host") && role !== "host") {
+    //     allowed = false;
+    // }
+    // if (router.pathname.startsWith("/admin") && role !== "admin") {
+    //     allowed = false;
+    // }
     const ComponentToRender = allowed ? Component : Home;
 
     return (
