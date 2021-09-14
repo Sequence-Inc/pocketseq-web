20be86bfde10fd44adfb8215c443ee465d220090
vnshgrg <ghale.avinash@gmail.com>
2021-09-14 19:15:11 +0900

    fixed minor bug

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -108,12 +108,11 @@ const HostDashboard = ({ currentSession }) => {
                                             <a
                                                 key={tab.name}
                                                 href={tab.href}
-                                                className={classNames(
+                                                className={
                                                     tab.current
-                                                        ? "border-primary text-gray-900"
-                                                        : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                                                    "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
-                                                )}
+                                                        ? "border-primary text-gray-900 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                                                        : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                                                }
                                                 aria-current={
                                                     tab.current
                                                         ? "page"
