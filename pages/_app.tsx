904931d5e4444a5f8f0878672fa99ecd871094f9
vnshgrg <ghale.avinash@gmail.com>
2021-10-24 22:13:57 +0900

    moved viewport metatag from _document to _app because nextjs said so

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { useRouter } from "next/router";
 // tailwind css
 import "@style/globals.css";
@@ -26,6 +27,12 @@ function MyApp({ Component, pageProps }: AppProps) {
 
     return (
         <ApolloProvider client={apolloClient}>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                />
+            </Head>
             <Component {...pageProps} />
         </ApolloProvider>
     );
