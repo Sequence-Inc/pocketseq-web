30712be5929d0e84bd8dc6ae24c62e1aa65ae229
vnshgrg <ghale.avinash@gmail.com>
2021-10-24 21:32:54 +0900

    added gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
-import Head from "next/head";
+import { useRouter } from "next/router";
 // tailwind css
 import "@style/globals.css";
 import { ApolloProvider } from "@apollo/client";
@@ -8,22 +9,23 @@ import { useApollo } from "src/apollo/apollo";
 function MyApp({ Component, pageProps }: AppProps) {
     const apolloClient = useApollo(pageProps.initialApolloState);
 
+    const router = useRouter();
+
+    const handleRouteChange = (url) => {
+        window.gtag("config", "G-5WKZPFK2LT", {
+            page_path: url,
+        });
+    };
+
+    useEffect(() => {
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
+
     return (
         <ApolloProvider client={apolloClient}>
-            <Head>
-                <meta
-                    name="viewport"
-                    content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-                />
-
-                {`<script async src="https://www.googletagmanager.com/gtag/js?id=G-5WKZPFK2LT"></script>
-                <script>
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-5WKZPFK2LT');
-                </script>`}
-            </Head>
             <Component {...pageProps} />
         </ApolloProvider>
     );
