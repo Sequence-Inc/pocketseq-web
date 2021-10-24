fa1ebaea63156cfcdf6fe2c927c7543e1f945132
vnshgrg <ghale.avinash@gmail.com>
2021-10-24 21:26:45 +0900

    added press release

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,14 @@ function MyApp({ Component, pageProps }: AppProps) {
                     name="viewport"
                     content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
                 />
+
+                {`<script async src="https://www.googletagmanager.com/gtag/js?id=G-5WKZPFK2LT"></script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-5WKZPFK2LT');
+                </script>`}
             </Head>
             <Component {...pageProps} />
         </ApolloProvider>
