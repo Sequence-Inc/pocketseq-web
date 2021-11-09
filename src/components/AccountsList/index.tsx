4b6d685dab8dbc6e14ba88d63822aff3955a235f
vnshgrg <ghale.avinash@gmail.com>
2021-11-09 20:23:20 +0900

    fixe allaccounts query to adhere to pagination

--- src/components/AccountsList/index.tsx
+++ src/components/AccountsList/index.tsx
@@ -61,12 +61,12 @@ export const AccountsList = ({ filterOptions }) => {
 
     if (error) return <NetworkHelper type="error" message={error.message} />;
 
-    if (data.allAccounts.length === 0) {
+    if (data.allAccounts.data.length === 0) {
         return <NetworkHelper type="no-data" />;
     }
 
     // return null;
-    const normalizedForm = data.allAccounts.map((account) => {
+    const normalizedForm = data.allAccounts.data.map((account) => {
         const newAccountData = { ...account };
         if (account.__typename === "UserProfile") {
             newAccountData.name = `${account.firstName} ${account.lastName}`;
