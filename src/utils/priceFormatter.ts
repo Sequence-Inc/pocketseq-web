69ca49f8152b05335e8eee4ab6b3328b1f00be63
vnshgrg <ghale.avinash@gmail.com>
2023-03-10 11:00:48 +0900

    updated subscription text

--- src/utils/priceFormatter.ts
+++ src/utils/priceFormatter.ts
@@ -1,9 +1,13 @@
 export const PriceFormatter = (amount: number) => {
-    const formatter = new Intl.NumberFormat("ja-JP", {
-        style: "currency",
-        currency: "JPY",
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 0,
-    });
-    return formatter.format(amount);
+    if (Platform.OS === "ios") {
+        const formatter = new Intl.NumberFormat("ja-JP", {
+            style: "currency",
+            currency: "JPY",
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0,
+        });
+        return formatter.format(amount);
+    } else {
+        return amount;
+    }
 };
